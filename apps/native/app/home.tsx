dcbd86fe4c58078f0d2ec2634d9999a9ba0987d5
Caio Peres <caioperescs@hotmail.com>
2024-10-16 12:09:58 -0300

    add gluestack-ui

--- apps/native/app/home.tsx
+++ apps/native/app/home.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@repo/components';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
@@ -5,6 +6,11 @@ export function Home() {
   return (
     <View className="flex-1 bg-blue-400 items-center justify-center">
       <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Box className="w-12 h-12 bg-red-300 justify-center items-center">
+        <Text>BOX</Text>
+      </Box>
+
       <StatusBar style="auto" />
     </View>
   );
--- apps/native/tailwind.config.js
+++ apps/native/tailwind.config.js
@@ -1,10 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const config = require('../../tailwind.config')
+
+module.exports = config
